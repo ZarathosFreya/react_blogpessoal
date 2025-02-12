f719f29921563bd4434412ee82392a2e323f8efa
ZarathosFreya <dessa.neves99@gmail.com>
2025-02-12 15:35:29 -0300

    :tada: Commit Inicial

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,41 @@
+
+
+function Home() {
+  return (
+    <div style={{
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+
+    }}>
+        <div>
+            <div style={{
+                width: "80vw",
+                display:"flex",
+                flexDirection: "column",
+                alignItems: "center",
+            }}>
+                <h2>Seja Bem Vinde!</h2>
+                <p>Expresse aqui seus pensamentos e opiniões</p>
+                <p>E embarque nessa jornada de aprendizado e evolução</p>
+            </div>
+            <div
+                    style={{
+                        width: '80vw',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <img
+                        src="https://i.imgur.com/VpwApCU.png"
+                        alt="Imagem da Página Home"
+                        width="400px"
+                    />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
